9007bcf2f43f0294836c311ef424a4388fb3a85b
unknown <acascon13@gmail.com>
2014-11-28 14:11:06 +0100

    Implementado el metodo printLastMailItem que nos devuelve el ultimo mensaje recibido

--- MailClient.java
+++ MailClient.java
@@ -10,53 +10,76 @@ public class MailClient
     // instance variables - replace the example below with your own
     private MailServer server;
     private String user;
+    private MailItem lastEmail;
 
     /**
      * Constructor for objects of class MailClient
      */
     public MailClient(MailServer server, String user)
     {
-       this.server = server;
-       this.user = user;
+        this.server = server;
+        this.user = user;
     }
 
     public MailItem getNextMailItem()
     {
+        lastEmail = server.getNextMailItem(user);
         return server.getNextMailItem(user);
     }
-    
+
     public void printNextMailItem()
     {
-        MailItem itemAux = server.getNextMailItem(user);
+         lastEmail = server.getNextMailItem(user);
         
-        if( itemAux != null)
+        if( lastEmail != null)
         {
-            itemAux.print();
+            lastEmail.print();
         } else
         {
             System.out.println("No hay mensaje en la bandeja");
         }
-        
-        
+
         
     }
+
     public void sendMailItem( String who, String message, String subject)
     {
         MailItem email = new MailItem (user, who, message, subject);
         server.post(email);
     }
-    
-    public int howManyMailItems()
+
+    public void howManyMailItems()
     {
-        return server.howManyMailItems(user);
+
+        int numberOfMails = server.howManyMailItems(user);
+        System.out.println("Numero de emails en el servidor: " + numberOfMails);
     }
+
     public void getNextMailItemAndAutorespond()
     {
         MailItem email = server.getNextMailItem(user);
-        String from = email.getFrom();
-        String subject = "RE: " + email.getSubject();
-        String message = "Estoy de vacaciones \n" + email.getMessage();
-        MailItem autoReply = new MailItem(user, from, subject, message);
-        server.post(autoReply);
+        if (email != null)
+        {
+            String to = email.getFrom();
+            String subject = "RE: " + email.getSubject();
+            String message = "Estoy de vacaciones \n" + email.getMessage();
+            MailItem autoReply = new MailItem(to, user, subject, message);
+            server.post(autoReply);
+        }
+
+        
+    }
+    public void printLastEmail()
+    {
+        
+       
+        if( lastEmail == null)
+        {
+            System.out.println("No se ha recibido ningun mensaje");
+        }
+        else {
+            lastEmail.print();
+        }
+        
     }
 }
